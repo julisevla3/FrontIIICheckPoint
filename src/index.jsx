d7a996d74ebcc2e292f257ca879857c9099cf3a0
Vinicius Evandro <vinnievandro@hotmail.com>
2022-12-12 21:06:21 -0300

    rotas

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import Navbar from "./Components/Navbar";
 import Home from "./Routes/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginForm from "./Components/LoginForm";
+import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import "./index.css";
 
@@ -12,7 +14,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Navbar />
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Login" element={<LoginForm />} />
+        <Route path="/Details" element={<details />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
     <Footer />
   </React.StrictMode>
 );
