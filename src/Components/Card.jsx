1425a4607bda48057a9899becf928ccfd12ea59f
Juliane Alves <41329356+julisevla3@users.noreply.github.com>
2022-12-14 01:01:49 -0300

    Delete card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -92,34 +92,8 @@ const Card = () => {
           </a>
         </div>
       </div>
-      <div className={`card`}>
-        <img
-          className="card-img-top"
-          src="/images/doctor.jpg"
-          alt="doctor placeholder"
-        />
-        <div className={`card-body ${styles.CardBody}`}>
-          {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
-          que vem da API */}
-          <a href={`/dentist/MatriculaDoDentista`}>
-            <h5 className={`card-title ${styles.title}`}>Nome e Sobrenome do dentista</h5>
-          </a>
-        </div>
-      </div>
-      <div className={`card`}>
-        <img
-          className="card-img-top"
-          src="/images/doctor.jpg"
-          alt="doctor placeholder"
-        />
-        <div className={`card-body ${styles.CardBody}`}>
-          {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
-          que vem da API */}
-          <a href={`/dentist/MatriculaDoDentista`}>
-            <h5 className={`card-title ${styles.title}`}>Nome e Sobrenome do dentista</h5>
-          </a>
-        </div>
-      </div>
+   
+     
 
 
 
