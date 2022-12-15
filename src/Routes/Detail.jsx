9c898cb9911286ef9959a6b5fc851aed289f57b8
Vinicius Evandro <vinnievandro@hotmail.com>
2022-12-15 19:25:07 -0300

    Home e detail

--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,32 +1,10 @@
-import DetailCard from "../Components/Details/DetailCard";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Navbar from "../Components/Navbar/Navbar";
+import DetailCard from "../Components/DetailCard/DetailCard";
 
 const Detail = () => {
-  const { matricula } = useParams();
-  const [dentista, setDentista] = useState({});
-
-  useEffect(() => {
-    getDentista();
-  }, []);
-
-  async function getDentista() {
-    try {
-      fetch(`https://dhodonto.ctdprojetos.com.br/dentista?matricula=${matricula}`)
-        .then((response) => response.json())
-        .then((data) => setDentista(data))
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
   return (
     <>
-    <Navbar />
-        <DetailCard {...dentista} key={dentista.matricula}/>
+      <DetailCard />
     </>
-
   )
 }
 
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,35 +1,22 @@
-
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Card from "../Components/Card/Card";
-import Navbar from "../Components/Navbar/Navbar";
 
 const Home = () => {
 
-  const [dentista, setDentista] = useState([]);
-
   useEffect(() => {
-    try {
-      fetch("https://dhodonto.ctdprojetos.com.br/dentista")
-        .then((response) => response.json())
-        .then((data) => setDentista(data))
-    } catch (error) {
-      console.log(error);
-    }
-    
+    //Nesse useEffect, deverá ser obtido todos os dentistas da API
+    //Armazena-los em um estado para posteriormente fazer um map
+    //Usando o componente <Card />
   }, []);
+
   return (
     <>
-      <Navbar />
       <h1>Home</h1>
-      <div className="card-grid container {theme}" >
-      {dentista.length ? dentista.map((dentista) => (
-            <Card {...dentista} key={dentista.matricula} />
-          ))
-        : null}
+      <div className="card-grid container">
+        <Card />
       </div>
     </>
   );
 };
+
 export default Home;
-
-
