6c8cc7c5c3e1d26a4a6a241b24dde22629b34653
Vinicius Evandro <vinnievandro@hotmail.com>
2022-12-15 19:31:32 -0300

    ajustes

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -6,7 +6,6 @@ import { useState } from "react";
 import { LoginContext } from "../../Providers/ProviderLogin";
 import { ThemeContext } from "../../Providers/ProviderTheme"
 import { useContext } from "react";
-import { Link } from "react-router-dom";
 
 const Card = () => {
   const { useToken } = useContext(LoginContext);
@@ -35,32 +34,10 @@ const Card = () => {
 
   return (
     <>
-<<<<<<< HEAD
       {/*1. Na linha seguinte deverá ser feito um teste se a aplicação
           está em dark mode e deverá utilizar o css correto */}
       {/* 2. Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
             que vem da API */}
-=======
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar o css correto */}
-      <div className={`card ${theme === 'light' ? '' : styles.cardDark}`} id={matricula}>
-        <img
-          className="card-img-top"
-          src="/images/doctor.jpg"
-          alt="doctor placeholder"
-        />
-        <div className={`card-body ${styles.CardBody}`}>
-                 
-          {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
-          que vem da API */}
-          <Link to={`/Details/${matricula}`}>
-            <h5 className={`card-title ${styles.title} `}>
-              {nome} {sobrenome}   
-             </h5>
-          </Link>
-        </div>
-      </div>
->>>>>>> 3759b34518451e7a8966b0364ac8ab3ddcd6d7eb
 
         {
           dentistas.map((dentista) => (
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,9 @@
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
-<<<<<<< HEAD
 import { LoginContext } from "../../Providers/ProviderLogin"
 import { ThemeContext } from "../../Providers/ProviderTheme";
 import { useEffect } from "react";
-=======
-import { Link, useNavigate } from "react-router-dom";
-
-
->>>>>>> 3759b34518451e7a8966b0364ac8ab3ddcd6d7eb
 
 const Navbar = () => {
   const { useToken, limparToken } = useContext(LoginContext);
@@ -39,7 +33,6 @@ const Navbar = () => {
             className="collapse navbar-collapse justify-content-end"
             id="navbarsExample03"
           >
-<<<<<<< HEAD
             <ul className="navbar-nav mb-2 mb-sm-0">
               <li className={
                 `nav-item 
@@ -87,67 +80,6 @@ const Navbar = () => {
                 </button>
               </li>
             </ul>
-=======
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-
-          <div>
-
-            <div
-
-              className="collapse navbar-collapse justify-content-end  " id="navbarsExample03"
-            >
-              <ul className="navbar-nav mb-2 mb-sm-0">
-                <li className={`nav-item ${styles.navBarLink}`}>
-                  {/* Ao clicar, o usuário deve ser redirecionado a home, com react-router */}
-                  <Link className="nav-link" to="/home">
-                    Home
-                  </Link>
-                </li>
-                <li className={`nav-item ${styles.navBarLink}`}>
-                  {/* Se o usuário estiver logado, deverá aparecer um botão de logout
-                que vai apagar o token do localstorage.
-                Se o usuário estiver deslogado, um link fará um redirecionamento, com react-router,
-                ao formulário de login
-                O botão de logout deverá ser testado darkmode
-                se sim, btn-dark, se não, btn-light */}
-                  {  
-                  localStorage.getItem("token") ?
-                    <button
-                    onClick={() => logout()}
-                    className={`btn btn-${theme === 'light' ? "light" : "dark"}`}
-                    
-                    >Logout
-                    </button>
-                  :
-                    <Link  className="nav-link" to="/login">
-                      Login
-                    </Link> 
-                    
-                                
-                }
-
-
-                </li>
-                <li className={`nav-item`}>
-                  {/* Ao ser clicado, esse botão mudará a aplicação para dark mode ou light mode.
-                 Lembre-se de usar um estado no contexto para fazer essa alteração.
-                 Na linha seguinte deverá ser feito um teste se a aplicação
-                 está em dark mode e deverá utilizar o icone ☀ ou 🌙 e btn-dark ou btn-light*/}
-
-                  <button onClick={handleTheme} className={`btn ${theme === 'light' ? 'btn-dark' : 'btn-light'} ${styles.btnStyle}`}
-
-                  >
-                    {theme === 'light' ? '🌙' : '☀'} {" "}
-                  </button>
-
-
-
-                </li>
-              </ul>
-            </div>
->>>>>>> 3759b34518451e7a8966b0364ac8ab3ddcd6d7eb
           </div>
         </div>
       </nav>
